c8506f2faee6c4bf5eaf84c2f2940fde596bcfee
muminvarici <mumin.varici@outlook.com>
2024-01-03 02:07:39 +0300

    #feat single server scenario implemented

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -1,4 +1,5 @@
-﻿using Integration.Service;
+﻿using Integration.Common;
+using Integration.Service;
 
 namespace Integration;
 
@@ -7,16 +8,16 @@ public abstract class Program
     public static void Main(string[] args)
     {
         var service = new ItemIntegrationService();
-        
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("a")));
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("b")));
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("c")));
 
         Thread.Sleep(500);
 
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("a")));
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("b")));
+        ThreadPool.QueueUserWorkItem(_ => PrintResult(service.SaveItem("c")));
 
         Thread.Sleep(5000);
 
@@ -25,5 +26,10 @@ public abstract class Program
         service.GetAllItems().ForEach(Console.WriteLine);
 
         Console.ReadLine();
+    }
+
+    private static void PrintResult(Result result)
+    {
+        Console.WriteLine("{0} - {1}", result.Success, result.Message);
     }
 }
--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,4 +1,5 @@
-﻿using Integration.Common;
+﻿using System.Collections.Concurrent;
+using Integration.Common;
 using Integration.Backend;
 
 namespace Integration.Service;
@@ -12,8 +13,24 @@ public sealed class ItemIntegrationService
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
+    
+    private HashSet<string> uniqueContents = new();
+    private readonly object lockObject = new();
+    
     public Result SaveItem(string itemContent)
     {
+        // Interlocked provides a lock mechanism for numbers but we need another lock mechanism  
+        lock (lockObject)//pesimistic lock
+        {
+            // Check if the content is in the hashset
+            if (uniqueContents.Contains(itemContent))
+            {
+                return new Result(false, $"Duplicate item received with content {itemContent}.");
+            }
+
+            uniqueContents.Add(itemContent);
+        }
+        
         // Check the backend to see if the content is already saved.
         if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
         {
